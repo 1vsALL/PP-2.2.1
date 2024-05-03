a5f187b601136ce4c630aa6e168daf713d81e1eb
1vsALL <50640956+1vsALL@users.noreply.github.com>
2024-05-03 17:25:32 +0300

    Update Car.java

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cars")
+@Table(name = "car")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
